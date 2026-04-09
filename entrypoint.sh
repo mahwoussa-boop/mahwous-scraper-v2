0a250c45ceab75141d69cee5ce61a2cc11c0ff81
mahwoussa-boop <mahwoussa@gmail.com>
2026-04-09 04:14:47 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-exec streamlit run app.py \
-  --server.port=${PORT:-8501} \
-  --server.address=0.0.0.0 \
-  --server.headless=true \
-  --server.enableCORS=false \
-  --server.enableXsrfProtection=false
